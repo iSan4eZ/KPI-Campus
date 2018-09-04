e5c95b85e18ebbb45be64bebbf40a2779212f46c
iSan4eZ <temnyip@gmail.com>
2018-09-04 16:58:03 +0300

    Initial commit

---
+++ src/main/java/weeks/WeeksApplication.java
@@ -0,0 +1,12 @@
+package com.twowings.weeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeeksApplication.class, args);
+  }
+}
---
+++ src/test/java/weeks/WeeksApplicationTests.java
@@ -0,0 +1,18 @@
+package com.twowings.weeks;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class WeeksApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
